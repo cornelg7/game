95e5b41242ce8a9d309c4537eb64e619e0e006e6
Cornel Goran <cornelg7@yahoo.com>
2018-05-16 19:10:35 +0100

    :doughnut: Designed level 0

--- js/Utilities.js
+++ js/Utilities.js
@@ -28,3 +28,37 @@
     function dressObject(obj, textureName) {
       obj.material = getMaterialFromTextures(textureName);
     }
+
+    function drawLine(group, pos) {
+      var mat = new THREE.MeshNormalMaterial();
+      var geo = new THREE.BoxGeometry(2, 0.5, 0.5);
+      var mes = new THREE.Mesh( geo, mat );
+      toDispose.push(mat);
+      toDispose.push(geo);
+      mes.position.copy(pos);
+      console.log(mes.position);
+      levelNumberGroups[group].add(mes);
+    }
+
+    function drawNumbersLevel0() {
+      for (i = 1; i <= 4; i++) levelNumberGroups.push(new THREE.Group());
+      drawLine(1, new THREE.Vector3(0, 0, 0), 1);
+      levelNumberGroups[1].position.copy(new THREE.Vector3(12, 4, 1));
+      levelNumberGroups[1].rotation.z = Math.PI/2;
+      levelNumberGroups[1].rotation.x = Math.PI/2;
+
+      drawLine(2, new THREE.Vector3(0, -0.5, 0), 1);
+      drawLine(2, new THREE.Vector3(0, 0.5, 0), 1);
+      levelNumberGroups[2].position.copy(new THREE.Vector3(12, 36, 1));
+      levelNumberGroups[2].rotation.z = Math.PI/2;
+      levelNumberGroups[2].rotation.x = Math.PI/2;
+
+      drawLine(3, new THREE.Vector3(0, -1, 0), 1);
+      drawLine(3, new THREE.Vector3(0, 0, 0), 1);
+      drawLine(3, new THREE.Vector3(0, 1, 0), 1);
+      levelNumberGroups[3].position.copy(new THREE.Vector3(28, 20, 1));
+      levelNumberGroups[3].rotation.y = Math.PI/2;
+      levelNumberGroups.forEach(function(x){
+        scene.add(x);
+      });
+    }
